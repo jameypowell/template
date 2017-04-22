c8e946d13f19379fdc7f0773c919ba3852455426
Jamey Powell <jpowell@adobe.com>
2017-04-22 00:21:44 -0600

    fixing pic

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Vote for Kya!!!</title>
+  <meta charset="UTF-8">
+  <script src='js/jquery.min.js'></script>
+  <script src='js/bootstrap.min.js'></script>
+  <link rel='stylesheet' href='css/bootstrap.min.css'>
+  <link rel='stylesheet' href='css/style.css'>
+</head>
+
+<?php
+?>
+<body>
+  <img class="fullimage"src="images/jp_slack.jpg">
+</body>
+
+</html>
