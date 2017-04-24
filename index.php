76c4db72cefbceee8d68b126fdd0cd0251a057a9
Jamey Powell <jpowell@adobe.com>
2017-04-23 23:43:06 -0600

    adding header and footer

--- index.php
+++ index.php
@@ -10,11 +10,22 @@
   <link rel='stylesheet' href='css/style.css'>
 </head>
 <body>
-  <img class="fullimage"src="images/hello.jpg">
-  <img class="fullimage"src="images/anderHappy.jpg">
-  <img class="fullimage"src="images/batslap.jpg">
-  <img class="fullimage"src="images/100bill.jpg">
-  <img class="fullimage"src="images/cat_lemons.jpg">
+  <div class="page-header text-center">
+    <div style="padding-top:1%;font-size:35px;font-weight:bold;">VOTE FOR KYA POWELL</div>
+    <div style="padding-bottom:1%;font-size:25px;font-style:italic;">Salem Junior High Student Council</div>
+  </div>
+  <div class="container-fluid">
+    <img class="fullimage"src="images/hello.jpg">
+    <img class="fullimage"src="images/anderHappy.jpg">
+    <img class="fullimage"src="images/batslap.jpg">
+    <img class="fullimage"src="images/100bill.jpg">
+    <img class="fullimage"src="images/cat_lemons.jpg">
+  </div>
+  <footer class="footer">
+      <div class="text-center">
+        <div>Send your catchy meme about voting for Kya to kyampowell@gmail.com and if we like it, we'll put it up.</div>
+      </div>
+    </footer>
 </body>
 
 </html>
