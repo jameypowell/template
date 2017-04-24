67a0770f62b1c25d9edf79948d321ec5b321cd7a
Jamey Powell <jpowell@adobe.com>
2017-04-23 22:55:37 -0600

    adding pics

--- index.php
+++ index.php
@@ -10,11 +10,11 @@
   <link rel='stylesheet' href='css/style.css'>
 </head>
 <body>
+  <img class="fullimage"src="images/hello.jpg">
   <img class="fullimage"src="images/anderHappy.jpg">
   <img class="fullimage"src="images/batslap.jpg">
   <img class="fullimage"src="images/100bill.jpg">
   <img class="fullimage"src="images/cat_lemons.jpg">
-  <img class="fullimage"src="images/vicVotes.jpg">
 </body>
 
 </html>
