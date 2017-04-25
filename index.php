831681a5f4c26805bc5f91c21b39096936871125
Jamey Powell <jpowell@adobe.com>
2017-04-25 07:09:30 -0600

    updating

--- index.php
+++ index.php
@@ -17,11 +17,11 @@
     <div style="padding-bottom:1%;font-size:25px;color:red;">Memes Updated Daily</div>
   </div>
   <div class="container-fluid">
-    <img class="img-responsive"src="images/hello.jpg">
-    <img class="img-responsive"src="images/anderHappy.jpg">
-    <img class="img-responsive"src="images/batslap.jpg">
-    <img class="img-responsive"src="images/100bill.jpg">
-    <img class="img-responsive"src="images/cat_lemons.jpg">
+    <img class="img-responsive"src="images/soshouldyou.jpg">
+    <img class="img-responsive"src="images/shes52.jpg">
+    <img class="img-responsive"src="images/twocats.jpg">
+    <img class="img-responsive"src="images/vicVotes.jpg">
+    <img class="img-responsive"src="images/loseVotes.jpg">
     <img class="img-responsive"src="images/braceYourself.jpg">
   </div>
   <footer class="footer">
